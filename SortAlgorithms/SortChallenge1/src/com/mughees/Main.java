94693b0e367c2886748bf90181dcd98c20497b8d
mughees-asif <mughees460@gmail.com>
2020-04-13 17:49:45 +0100

    SortChallenge1 completed

--- SortAlgorithms/SortChallenge1/src/com/mughees/Main.java
+++ SortAlgorithms/SortChallenge1/src/com/mughees/Main.java
@@ -13,7 +13,6 @@ public class Main {
     }
 
     static void mergeSort(int[] input, int start, int end) {
-
         if (end - start < 2) {
             return;
         }
@@ -26,8 +25,7 @@ public class Main {
     }
 
     static void merge(int[] input, int start, int mid, int end) {
-
-        if(input[mid - 1] <= input[mid]){
+        if(input[mid - 1] >= input[mid]) {
             return;
         }
         int i = start;
@@ -37,11 +35,9 @@ public class Main {
         int[] temp = new int[end - start];
 
         while (i < mid && j < end) {
-            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+            temp[tempIndex++] = input[i] >= input[j] ? input[i++] : input[j++];
         }
-
         System.arraycopy(input, i, input, start + tempIndex, mid - i);
         System.arraycopy(temp, 0, input, start, tempIndex);
-
     }
 }
