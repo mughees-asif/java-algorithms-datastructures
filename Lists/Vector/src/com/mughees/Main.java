68ea2b8bc43dee9b7029f677f5127e52c148e091
mughees-asif <mughees460@gmail.com>
2020-04-19 13:54:48 +0100

    vectors

--- Lists/Vector/src/com/mughees/Main.java
+++ Lists/Vector/src/com/mughees/Main.java
@@ -1,8 +1,16 @@
 package com.mughees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 4567));
+        employeeList.add(new Employee("Mary", "Smith", 22));
+        employeeList.add(new Employee("Mike", "Wilson", 3245));
     }
 }
