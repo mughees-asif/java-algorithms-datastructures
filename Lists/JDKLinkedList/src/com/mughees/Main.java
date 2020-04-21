b2a58f0f64d9ce19ff134ba9c9a01d69c787349f
mughees-asif <mughees460@gmail.com>
2020-04-21 15:02:12 +0100

    JDKLinkedList

--- Lists/JDKLinkedList/src/com/mughees/Main.java
+++ Lists/JDKLinkedList/src/com/mughees/Main.java
@@ -11,20 +11,31 @@ public class Main {
         Employee johnDoe = new Employee("John", "Doe", 4567);
         Employee marySmith = new Employee("Mary", "Smith", 22);
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 87);
 
         LinkedList<Employee> list = new LinkedList<>();
+
+        // addFirst = adds an item to the start of the list
         list.addFirst(janeJones);
         list.addFirst(johnDoe);
         list.addFirst(marySmith);
         list.addFirst(mikeWilson);
+        printMethod(list);
 
-        // print the list
+        // add = adds an item to the end of list
+        list.add(billEnd);
+        printMethod(list);
+    }
+
+    // print the list
+    public static void printMethod (LinkedList list) {
         Iterator iter = list.iterator();
         System.out.println("HEAD -> ");
         while (iter.hasNext()) {
             System.out.print(iter.next());
-            System.out.println("<=>");
+            System.out.println(" <=> ");
         }
         System.out.println("null");
+        System.out.println("===========================");
     }
 }
