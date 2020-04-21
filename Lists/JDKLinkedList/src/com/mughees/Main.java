dbe60e6ce56f0e79c8fb3fbd1ce5d694a25d1a01
mughees-asif <mughees460@gmail.com>
2020-04-21 15:06:36 +0100

    JDKLinkedList

--- Lists/JDKLinkedList/src/com/mughees/Main.java
+++ Lists/JDKLinkedList/src/com/mughees/Main.java
@@ -22,13 +22,21 @@ public class Main {
         list.addFirst(mikeWilson);
         printMethod(list);
 
-        // add = adds an item to the end of list
+        // add / addLast = adds an item to the end of list
         list.add(billEnd);
+        printMethod(list);
+
+        // removeFirst = removes an item from the front of the list
+        list.removeFirst();
+        printMethod(list);
+
+        // removeLast = removes an item from the end of the list
+        list.removeLast();
         printMethod(list);
     }
 
     // print the list
-    public static void printMethod (LinkedList list) {
+    private static void printMethod (LinkedList list) {
         Iterator iter = list.iterator();
         System.out.println("HEAD -> ");
         while (iter.hasNext()) {
