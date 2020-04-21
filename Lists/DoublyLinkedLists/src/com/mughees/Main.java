b7acc532c687a43d68716e493db536bbf2ac84b6
mughees-asif <mughees460@gmail.com>
2020-04-21 14:25:49 +0100

    DoublyLinkedLists implementation

--- Lists/DoublyLinkedLists/src/com/mughees/Main.java
+++ Lists/DoublyLinkedLists/src/com/mughees/Main.java
@@ -3,6 +3,9 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
     }
 }
