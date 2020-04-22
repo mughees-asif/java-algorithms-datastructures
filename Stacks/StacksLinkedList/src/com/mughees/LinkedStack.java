d1fead87d7077c3ea95e3c66256f63a77ba8b179
mughees-asif <mughees460@gmail.com>
2020-04-22 16:11:05 +0100

    Stacks LinkedList implementation

---
+++ Stacks/StacksLinkedList/src/com/mughees/LinkedStack.java
@@ -0,0 +1,12 @@
+package com.mughees;
+
+import java.util.LinkedList;
+
+public class LinkedStack {
+
+    private LinkedList<Employee> stack;
+
+    public LinkedStack() {
+        stack = new LinkedList<Employee>();
+    }
+}
