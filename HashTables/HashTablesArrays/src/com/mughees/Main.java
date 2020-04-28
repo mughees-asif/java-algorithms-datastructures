2da924a8724be2f5c7e56ae1a899d2e3973da909
mughees-asif <mughees460@gmail.com>
2020-04-28 16:17:17 +0100

    hashtables

--- HashTables/HashTablesArrays/src/com/mughees/Main.java
+++ HashTables/HashTablesArrays/src/com/mughees/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        SimpleHashtable ht = new SimpleHashtable();
+        ht.put("Jones", janeJones);
     }
 }
