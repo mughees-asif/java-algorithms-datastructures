6120cef9e5c2c402ad90ecb03a06700dabccabb5
mughees-asif <mughees460@gmail.com>
2020-04-28 16:12:24 +0100

    hashtables

--- HashTables/HashTablesArrays/src/com/mughees/SimpleHashtable.java
+++ HashTables/HashTablesArrays/src/com/mughees/SimpleHashtable.java
@@ -1,13 +1,35 @@
 package com.mughees;
 
 public class SimpleHashtable {
+
     private Employee[] hashtable;
 
     public SimpleHashtable() {
         hashtable = new Employee[10];
     }
 
+    // simple hash table does not take into account collisions
+    public void put(String key, Employee employee) {
+        int hashKey = hashKey(key);
+        if (hashtable[hashKey] != null) {
+            System.out.println("An employee already exists at position: " + hashKey);
+        } else {
+            hashtable[hashKey] = employee;
+        }
+    }
+
+    public Employee get(String key) {
+        int hashKey = hashKey(key);
+        return hashtable[hashKey];
+    }
+
     private int hashKey(String key) {
         return key.length() % hashtable.length;
+    }
+
+    public void printHashtable() {
+        for (Employee employee : hashtable) {
+            System.out.println(employee);
+        }
     }
 }
