527482bcf84dd498884cd29502c7098592a061a0
mughees-asif <mughees460@gmail.com>
2020-05-05 17:11:50 +0100

    BucketSort implementation

--- SortAlgorithms/BucketSort/src/com/mughees/Main.java
+++ SortAlgorithms/BucketSort/src/com/mughees/Main.java
@@ -1,13 +1,15 @@
 package com.mughees;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] array = { 54, 46, 83, 66, 94, 92, 43 };
-
+        bucketSort(array);
         for (int value : array) {
             System.out.println(value);
         }
@@ -21,6 +23,17 @@ public class Main {
 
         for (int value : input) {
             buckets[hash(value)].add(value);
+        }
+
+        for(List bucket : buckets) {
+            Collections.sort(bucket);
+        }
+
+        int j = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            for (int value : buckets[i]) {
+                input[j++] = value;
+            }
         }
     }
 
