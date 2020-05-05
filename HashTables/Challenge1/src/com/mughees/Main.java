8a1e5ccc05e40fbbb8b0eafccf7e701a75efd367
mughees-asif <mughees460@gmail.com>
2020-05-05 17:20:46 +0100

    BucketSort implementation

--- HashTables/Challenge1/src/com/mughees/Main.java
+++ HashTables/Challenge1/src/com/mughees/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
         int[] nums = new int[10];
         int[] numsToAdd = { 59382, 43, 6894, 500, 99, -58 };
-        for (int i = 0; i < numsToAdd.length; i++) {
-            nums[hash(numsToAdd[i])] = numsToAdd[i];
+        for (int value : numsToAdd) {
+            nums[hash(value)] = value;
         }
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+        for (int num : nums) {
+            System.out.println(num);
         }
     }
 
     public static int hash(int value) {
-
+        return Math.abs(value % 10);
     }
 }
