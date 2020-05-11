91f4bbdfd662c6bb9519bfe7becaa571c558ec6c
mughees-asif <mughees460@gmail.com>
2020-05-11 13:26:34 +0100

    BinarySearch iterative

--- SearchAlgorithms/BinarySearch/src/com/mughees/Main.java
+++ SearchAlgorithms/BinarySearch/src/com/mughees/Main.java
@@ -3,6 +3,27 @@ package com.mughees;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] arr = new int[] {-22, -15, 1, 7, 20, 35, 55};
+        System.out.println(iterativeBinarySearch(arr, 55));
+
+    }
+
+    public static int iterativeBinarySearch(int[] input, int value) {
+        int start = 0;
+        int end = input.length;
+
+        while (start < end) {
+            int midpoint = (start + end) / 2;
+            System.out.println("midpoint: " + midpoint);
+            if (input[midpoint] == value) {
+                return midpoint;
+            } else if (input[midpoint] < value) {
+                start = midpoint + 1;
+            } else {
+                end = midpoint;
+            }
+        }
+        return -1;
     }
 }
