3fef2a87e35a1a0eb30e20a44b00bbd1cccf672c
mughees-asif <mughees460@gmail.com>
2020-05-11 13:34:56 +0100

    BinarySearch recursive

--- SearchAlgorithms/BinarySearch/src/com/mughees/Main.java
+++ SearchAlgorithms/BinarySearch/src/com/mughees/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = new int[] {-22, -15, 1, 7, 20, 35, 55};
-        System.out.println(iterativeBinarySearch(arr, 55));
+        System.out.println(iterativeBinarySearch(arr, 35));
+        System.out.println("==================================");
+        System.out.println(recursiveBinarySearch(arr, 35));
 
     }
 
@@ -16,14 +18,24 @@ public class Main {
         while (start < end) {
             int midpoint = (start + end) / 2;
             System.out.println("midpoint: " + midpoint);
-            if (input[midpoint] == value) {
-                return midpoint;
-            } else if (input[midpoint] < value) {
-                start = midpoint + 1;
-            } else {
-                end = midpoint;
-            }
+            if (input[midpoint] == value)       return midpoint;
+            else if (input[midpoint] < value)   start = midpoint + 1;
+            else                                end = midpoint;
         }
         return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] input, int value) {
+        return recursiveBinarySearch(input, 0, input.length, value);
+    }
+
+    public static int recursiveBinarySearch(int[] input, int start, int end, int value) {
+        if (start >= end)       return -1;
+        int midpoint = (start + end) / 2;
+        System.out.println("midpoint: " + midpoint);
+
+        if (input[midpoint] == value)       return midpoint;
+        else if (input[midpoint] < value)   return recursiveBinarySearch(input, midpoint+1, end, value);
+        else                                return recursiveBinarySearch(input, start, midpoint, value);
     }
 }
